41e74b227392b99812772810366be07d51f877d9
Sandaru Muthuwadige <37379922+sandxru@users.noreply.github.com>
2025-01-24 21:06:18 +0530

    Update page.tsx

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -1,6 +1,3 @@
-import JSZip from "jszip";
-import { useState } from "react";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-6 pb-16 gap-8 sm:p-16 font-[family-name:var(--font-geist-sans)]">
